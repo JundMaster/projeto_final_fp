bff7dbd1b816c33795b4ebe225c62305912b930c
JundMaster <gabrielsantostroll@gmail.com>
2020-02-06 16:13:07 +0000

    score.py cleaner

--- score.py
+++ score.py
@@ -2,10 +2,11 @@ def save_score(score):
     with open('score.txt', 'a') as score_file:
         score_file = open('score.txt', 'a')
         score_file.write('Score: ' + str(score) + '\n')
+
+def get_score():
     with open('score.txt', 'r') as score_file:
         temp_list = []
         score_list = []
-        j = 0
         number = []
         for line in score_file:
             for i in range(0, len(line)):
@@ -14,24 +15,6 @@ def save_score(score):
                 elif number:
                     temp_list.append(number)
                     number = []
-    score_file.close()
-    for i in range (0, len(temp_list)):
-        score_list.append(int("".join(temp_list[i])))
-
-def get_score():
-    score_file = open('score.txt', 'r')
-    temp_list = []
-    score_list = []
-    j = 0
-    number = []
-    for line in score_file:
-        for i in range(0, len(line)):
-            if line[i].isdigit():
-                number.append(line[i])
-            elif number:
-                temp_list.append(number)
-                number = []
-    score_file.close()
     for i in range (0, len(temp_list)):
         score_list.append(int("".join(temp_list[i])))
     return (score_list[-1], max(score_list))
